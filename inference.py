b81f1083892e2a8d7d8dcae48530c941a18b57c2
hanjaejun <wowns4690@kakao.com>
2024-09-30 14:37:38 +0900

    Cors fix

--- inference.py
+++ inference.py
@@ -1,6 +1,7 @@
 # app.py
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import PredictIn, PredictOut
 import joblib
 from mangum import Mangum
@@ -16,6 +17,19 @@ MODEL = get_model()
 # Create a FastAPI instance
 app = FastAPI()
 
+orgins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://tb43gljhbe.execute-api.ap-northeast-2.amazonaws.com/mlops"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=orgins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict", response_model=PredictOut)
 def predict(data: PredictIn) -> PredictOut:
